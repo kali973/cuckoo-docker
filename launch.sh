a2d74cba74b5229460ca304ce61f456799aca602
kali973 <giles.coutard@outlook.fr>
2023-07-14 16:51:12 +0200

    intialisation projet

--- launch.sh
+++ launch.sh
@@ -5,8 +5,8 @@ menu_py_path="$current_directory/services/menu.py"
 
 if [[ -f "$menu_py_path" ]]; then
   cd "$current_directory/services" || exit 1
-  pip3 install -r requirements.txt  # Installer les dépendances
-  chmod +x menu.py  # Changer les permissions pour le rendre exécutable
+  gnome-terminal -- /bin/bash -c "pip3 install -r requirements.txt; read -s -n 1 -p 'Appuyez sur une touche pour continuer...';" &
+  chmod +x menu.py
   sudo /usr/bin/python3 menu.py
 else
   echo "Le fichier menu.py n'a pas été trouvé dans le répertoire services."
